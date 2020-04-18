b617721212ff9f35df6fee9f0605e4fa0c714cb7
mma <michalmandlik@gmail.com>
2020-04-18 23:20:07 +0200

    sreality checker, not that bad so far

--- readWeb.py
+++ readWeb.py
@@ -1,6 +1,18 @@
 from bs4 import BeautifulSoup
-from requests import get
+from selenium import webdriver
 
-url = 'http://www.imdb.com/search/title?release_date=2017&sort=num_votes,desc&page=1'
-response = get(url)
-print(response.text[:500])
+driver = webdriver.Safari()
+
+# driver.get("https://www.sreality.cz/hledani/prodej/byty/brno?stari=mesic")
+driver.get(
+    "https://www.sreality.cz/hledani/prodej/byty/praha-7?velikost=1%2B1,2%2Bkk,2%2B1,3%2Bkk&stari=mesic&cena-od=0&cena-do=7000000&bez-aukce=1")
+soup = BeautifulSoup(driver.page_source, "html.parser")
+driver.quit()
+
+i = 0
+for title in soup.select(".text-wrap"):
+    i = i + 1
+    num = "https://www.sreality.cz" + title.select_one(".title").get('href')
+    print(num)
+
+print(i)
