05337dedeccfb15f1afcbac75cfe42142bc165d1
mma <michalmandlik@gmail.com>
2020-04-18 18:54:33 +0200

    add requests

--- readWeb.py
+++ readWeb.py
@@ -1,1 +1,6 @@
 from bs4 import BeautifulSoup
+from requests import get
+
+url = 'http://www.imdb.com/search/title?release_date=2017&sort=num_votes,desc&page=1'
+response = get(url)
+print(response.text[:500])
